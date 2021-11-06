940c53b9d36add029b012bdac5ee4bcb144be567
Aidan Hunt <aidan@aidanhunt.dev>
2021-11-06 13:51:13 -0400

    create routes

--- backend/app.py
+++ backend/app.py
@@ -6,3 +6,34 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# routes interacted with by fronted
+
+@app.get("/user/<uid>")
+def user_summary(uid):
+    # return user's overall info
+    return ""
+
+@app.get("/user/<uid>/limits")
+def user_limits(uid):
+    # return user's limit for each category and how much they have spent so far this month
+    return
+
+@app.post("/user/<uid>/request")
+def request_spending():
+    # decide whether or not to approve the request to spend more than limit
+    return
+
+# routes representing data coming from other bank systems
+# this is not going to be called by our API
+
+@app.post("/user/<uid>/transaction")
+def txn_authorize():
+    # return a response saying whether or not user should be allowed to make this purchase
+    # i.e. is it within their limits or do they have apporced request for it
+    return
+
+@app.get("/user/<uid>/payment")
+def accept_payment():
+    # reduce user's balance due
+    return
---
+++ backend/dummydata.py
@@ -0,0 +1,10 @@
+users = {
+    0: {'name': "John Doe", 'initial_debt': 10001, ''
+        'transactions': {
+            0: {'date': 20211101, 'amount': 100, 'merchant': "JOES GROCERIES", 'MCC': 5411},
+            1: {'date': 20211101, 'amount': 50, 'merchant': "BEST BUY", 'MCC': 5722}},
+        'preauthorized': {
+            # {'data_approved': 20211101, 'amount': 2000, 'category': , 'prepaid_amount': }
+            }
+        }
+}
