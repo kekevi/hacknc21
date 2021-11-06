5c984361a510a876c5856d6955dccd846541c23d
Aidan Hunt <aidan@aidanhunt.dev>
2021-11-06 15:10:17 -0400

    update backend structure

--- backend/dummydata.py
+++ backend/dummydata.py
@@ -1,10 +1,25 @@
+from datetime import date
+
 users = [
-    {'name': "John Doe", 'created_date': 20211101, 'initial_debt': 10001, ''
+    {'account': 1234, 'created_date': 20211101, 'initial_balance': 10000,
         'transactions': [
-            {'date': 20211101, 'amount': 100, 'merchant': "JOES GROCERIES", 'MCC': 5411},
-            {'date': 20211101, 'amount': 50, 'merchant': "BEST BUY", 'MCC': 5722}],
+            # last month
+            {'date': date(2021, 10, 27), 'amount': 100, 'merchant': "JOES GROCERIES", 'MCC': 5411},
+            {'date': date(2021, 10, 12), 'amount': 50, 'merchant': "BEST BUY", 'MCC': 5722},
+            # this month
+            {'date': date(2021, 11, 1), 'amount': 25, 'merchant': "JOES GROCERIES", 'MCC': 5722}
+            ],
         'preauthorized': [
-            # {'data_approved': 20211101, 'amount': 2000, 'category': , 'prepaid_amount': }
-        ]
-    }
-]
+            {'data_approved': date(2021, 10, 15), 'amount': 2000, 'category': 1}
+            ],
+        'payments': [
+            # last month
+            {'date': date(2021, 10, 15), 'amount': -100}
+            # this month
+            ]
+        }
+    ]
+
+categories = {
+    1: 5411
+}
